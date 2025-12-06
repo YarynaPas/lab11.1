47c7cc6bb718981f1ede48574f210be786aa4273
Пасічна Ярина <pas06chna@gmail.com>
2025-12-07 00:05:30 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace lab11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обов'язкова змінна конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+
+        /// <summary>
+        /// Очистка всіх використовуваних ресурсів.
+        /// </summary>
+        /// <param name="disposing">true, якщо керовані ресурси слід видалити; інакше false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматично створений конструктором форм Windows
+
+        /// <summary>
+        /// Обов'язковий метод для підтримки конструктора форм.
+        /// Не змінюйте його вміст у редакторі коду.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(387, 340);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 432);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Demo Delegate";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿// Це дещо видозмінений приклад із книги Троелсена для ілюстрування
+// поняття делегат і роботи з делегатами
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // --------- Клас Car ---------
+        public class Car
+        {
+            public int CurrentSpeed { get; set; }
+            public int MaxSpeed { get; set; }
+            public string PetName { get; set; }
+
+            private bool carIsDead;  // поле для перевірки, чи автомобіль не зламався
+
+            public Car()             // конструктор класу
+            {
+                MaxSpeed = 100;
+            }
+
+            public Car(string name, int maxSp, int currSp)  // конструктор з параметрами
+            {
+                MaxSpeed = maxSp;
+                CurrentSpeed = currSp;
+                PetName = name;
+            }
+
+            // Оголошення делегата у класі Car
+            public delegate void CarEngineHandler(string msgForCaller);
+
+            // Оголошення закритої змінної listOfHandlers типу делегат
+            CarEngineHandler listOfHandlers;
+
+            // Додавання методу для доступу до змінної listOfHandlers ззовні класу
+            public void RegisterWithCarEngine(CarEngineHandler metodToCall)
+            {
+                /* Змінній типу делегат присвоюємо метод, що має сигнатуру,
+                 * яка вказана при оголошенні делегата */
+                listOfHandlers = metodToCall;
+            }
+
+            /* Метод для зміни поточної швидкості автомобіля.
+             * Він буде викликати процес створення повідомлення і додавання його до тексту мітки.
+             * Залежно від швидкості автомобіля, будуть генеруватись різні повідомлення
+             */
+            public void Accselerate(int delta)
+            {
+                if (carIsDead)
+                {
+                    /* Змінна типу делегат listOfHandlers запускає метод, вказаний при її створенні
+                     * з параметрами, заданими в операторі звертання до змінної.
+                     * Перевіримо, чи передано метод у змінну listOfHandlers і
+                     * якщо так, то викликаємо метод, адреса якого записана у змінній listOfHandlers
+                     */
+                    if (listOfHandlers != null)
+                        listOfHandlers("Увага! Занадто велика швидкість!");
+                }
+                else
+                {
+                    CurrentSpeed += delta;
+
+                    // Перевіряємо, чи передано метод у змінну listOfHandlers, а також
+                    // чи не занадто велика швидкість і якщо так, то видаємо повідомлення
+                    if ((MaxSpeed - CurrentSpeed < 10) && listOfHandlers != null)
+                    {
+                        // Викликаємо метод, записаний у делегаті за допомогою змінної типу делегат
+                        // з новими параметрами
+                        listOfHandlers("Увага! Занадто велика швидкість!");
+                        listOfHandlers("На жаль автомобіль зламався");
+                    }
+                    else
+                    {
+                        if (CurrentSpeed >= MaxSpeed)
+                            carIsDead = true;
+                        else
+                            listOfHandlers?.Invoke("Поточна швидкість=" + CurrentSpeed.ToString());
+                    }
+                }
+            }
+        }
+
+        // Метод-обробник, який викликає делегат — виводить текст у label1
+        public void OnCarEngineEvent(string msg)
+        {
+            label1.Text = label1.Text + msg + " \n";
+        }
+
+        // Обробник кліку по кнопці Start
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = ""; // очищаємо перед новим запуском
+
+            // Створюємо об'єкт типу Car
+            Car myCar = new Car("Старенький Запорожець", 100, 0);
+
+            // Створимо змінну типу делегат і зашлемо в неї адресу методу,
+            // який буде викликатись через цю змінну
+            Car.CarEngineHandler myDelegat =
+                new Car.CarEngineHandler(OnCarEngineEvent);
+
+            /* Звернемось до методу RegisterWithCarEngine, щоб вказати метод,
+             * який повинен викликатись (зареєструвати)
+             */
+            myCar.RegisterWithCarEngine(myDelegat);
+
+            // Ми можемо викликати метод OnCarEngineEvent і поза делегатом
+            OnCarEngineEvent("Старенький Запорожець");
+            OnCarEngineEvent("Вмикаємо запалювання");
+
+            // Змінюємо швидкість автомобіля і відслідковуємо, що трапиться
+            for (int i = 0; i < 10; i++)
+                myCar.Accselerate(10);
+        }
+    }
+}
